773a36e77c1f2e9178aeff4969c8fef73149ab7c
Harmandeeep2312 <harman145singh@gmail.com>
2026-02-20 10:06:13 +0530

    Completed useCase 4

--- useCase1Palindrome.java
+++ useCase1Palindrome.java
@@ -5,18 +5,20 @@ public class useCase1Palindrome {
         System.out.println("Welcome To the Palindrome Checker Management System");
         System.out.println("Version: 1.0");
         System.out.println("System Initialised Successfully");
-                Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a word to check for Palindrome string or not");
-        String Word = sc.nextLine();
-        String str = "";
-        for (int i = Word.length()-1; i>=0; i--) {
-            str+= Word.charAt(i);
-            System.out.println(str);
+        String Word= sc.next();
+        int start =0;
+        int end = Word.length()-1;
+        Boolean isPalindrome= true;
+        while(start<end){
+            if(Word.charAt(start)!= Word.charAt(end)) isPalindrome=false;
+            start++;
+            end--;
         }
-        if (str.equals(Word))
-            System.out.println("Yess It is A Palindrome Number");
-        else
-            System.out.println("No it Is Not A Palindrome Number");
+        if(isPalindrome)System.out.println("Palindrome");
+        else System.out.println("not a palindrome");
+
             sc.close();
     }
 }
