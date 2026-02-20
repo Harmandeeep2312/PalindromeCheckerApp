8643c8254055663c6bf6cd4138a0ff2c3bea2941
Harmandeeep2312 <harman145singh@gmail.com>
2026-02-20 09:28:55 +0530

    Completed the useCase1

---
+++ PalindromeCheckerApp/useCase1Palindrome.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class useCase1Palindrome {
+    public static void main(String[] args) {
+        System.out.println("Welcome To the Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System Initialised Successfully");
+    }
+}
